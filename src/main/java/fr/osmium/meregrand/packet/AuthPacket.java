05ecbd5a8bae116573818eb0c791e83b55f37141
Xamez <34zemax34@gmail.com>
2022-12-02 03:53:24 +0100

    :sparkles: Mise en place du TGMPP

--- src/main/java/fr/osmium/meregrand/packet/AuthPacket.java
+++ src/main/java/fr/osmium/meregrand/packet/AuthPacket.java
@@ -5,12 +5,14 @@ public class AuthPacket extends Packet {
     private final String email;
     private final String password;
     private final String messageHash;
+    private final String targetMail;
 
     public AuthPacket(String messageHash, String email, String password, String targetMail) {
         super(PacketType.AUTH_PACKET);
         this.email = email;
         this.password = password;
         this.messageHash = messageHash;
+        this.targetMail = targetMail;
     }
 
     public String getEmail() {
@@ -23,5 +25,9 @@ public class AuthPacket extends Packet {
 
     public String getMessage() {
         return messageHash;
+    }
+
+    public String getTargetMail() {
+        return targetMail;
     }
 }
